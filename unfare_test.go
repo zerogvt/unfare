d919be30cd319923bd88fd6bfc9cab15a58b7d67
zerogvt <gkoumplias@gmail.com>
2021-11-06 15:52:43 +0200

    more UTs

--- unfare_test.go
+++ unfare_test.go
@@ -27,12 +27,35 @@ func TestVelocityZero(t *testing.T) {
 	}
 }
 
-func TestFareTo(t *testing.T) {
+func TestFareToDaily(t *testing.T) {
 	p1 := Point{1, Coordinates{37.910030364990234, 23.90641212463379}, 1405090726}
 	p2 := Point{1, Coordinates{37.93056106567383, 23.93911361694336}, 1405090858}
 	have, _ := p1.fareTo(&p2)
 	havex100 := int(math.Round(100 * have))
 	wantx100 := 271
+	// we don't want to compare floats
+	if wantx100 != havex100 {
+		t.Fatalf("TestFareTo: Wanted %d but got %d", wantx100, havex100)
+	}
+}
+
+func TestFareToNightly(t *testing.T) {
+	p1 := Point{1, Coordinates{37.910030364990234, 23.90641212463379}, 1636158734}
+	p2 := Point{1, Coordinates{37.93056106567383, 23.93911361694336}, 1636159814}
+	have, _ := p1.fareTo(&p2)
+	havex100 := int(math.Round(100 * have))
+	wantx100 := 477
+	if wantx100 != havex100 {
+		t.Fatalf("TestFareTo: Wanted %d but got %d", wantx100, havex100)
+	}
+}
+
+func TestFareToIdling(t *testing.T) {
+	p1 := Point{1, Coordinates{37.910030364990234, 23.90641212463379}, 1636158734}
+	p2 := Point{1, Coordinates{37.93056106567383, 23.90641212463379}, 1636159814}
+	have, _ := p1.fareTo(&p2)
+	havex100 := int(math.Round(100 * have))
+	wantx100 := 357
 	if wantx100 != havex100 {
 		t.Fatalf("TestFareTo: Wanted %d but got %d", wantx100, havex100)
 	}
@@ -48,5 +71,13 @@ func TestFareToWeedsOutlierPoint(t *testing.T) {
 	}
 	if int(have) != 0 {
 		t.Fatalf("TestFareTo did not return zero fair for outlier point")
+	}
+}
+
+func TestNewPoint(t *testing.T) {
+	line := "1, 1.11, 2.22, 333"
+	p := NewPoint(line)
+	if p.Id_ride != 1 || int(math.Round(p.Coord.Lat*100)) != 111 || int(math.Round(p.Coord.Lon*100)) != 222 || p.Ts != 333 {
+		t.Fatalf("Expected point %s but got %s", line, p)
 	}
 }
